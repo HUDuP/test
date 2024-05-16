ace2d0088e5ec2cc35d230d6b2ed8ae47d15a411
Acharayu <acharayu.r@arise.tech>
2024-05-16 17:28:32 +0700

    test

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"bank/service"
 	"fmt"
 	"net/http"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -14,19 +15,9 @@ import (
 )
 
 func main() {
+	initTimezone()
 	initConfig()
-	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?parseTime=true",
-		viper.GetString("db.username"),
-		viper.GetString("db.password"),
-		viper.GetString("db.host"),
-		viper.GetInt("db.port"),
-		viper.GetString("db.database"),
-	)
-	
-	db, err := sqlx.Open(viper.GetString("db.driver"), dsn)
-	if err != nil {
-		panic(err)
-	}
+	db := initDatabase()
 
 	customerRepository := repository.NewCustomerRepositoryDB(db)
 	customerRepositoryMock := repository.NewCustomerRepositoryMock()
@@ -52,4 +43,35 @@ func initConfig() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func initTimezone() {
+	ict,err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		panic(err)
+	}
+
+	time.Local = ict
+}
+
+func initDatabase() *sqlx.DB {
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?parseTime=true",
+		viper.GetString("db.username"),
+		viper.GetString("db.password"),
+		viper.GetString("db.host"),
+		viper.GetInt("db.port"),
+		viper.GetString("db.database"),
+	)
+	
+	db, err := sqlx.Open(viper.GetString("db.driver"), dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetConnMaxLifetime(3 * time.Minute)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return db
+
 }
